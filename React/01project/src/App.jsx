5a389facce375db67f63946e63cf17ac71e2d3ed
Abhinav Kant <abhinavkant7071@gmail.com>
2025-06-22 12:27:43 +0530

    changes

---
+++ React/01project/src/App.jsx
@@ -0,0 +1,21 @@
+import Chai from './chai'
+function App() {
+  const Abhinav = "lord Abhinav himself";
+
+  return (
+    //<Chai/>
+    // <h1>Here we are</h1>
+    //it is because  we can return only one tag in tge jsx files
+    
+
+    //So we can do this like
+    <>
+    <h1>I am the {Abhinav}</h1>
+    <Chai/>
+    </>
+
+
+  )
+}
+
+export default App
---
+++ React/01project/src/chai.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return (
+        <h3> I am the best</h3>
+    )
+}
+export default Chai
